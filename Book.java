0fa0d0f094a4ab95ba6d2a8da58f83d691b60d68
Luis Monterroso <43154683+lmonterroso@users.noreply.github.com>
2018-09-17 21:09:16 -0400

    added paramters and methods

--- Book.java
+++ Book.java
@@ -11,16 +11,87 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int borrowed;
+    private boolean courseText;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int numPages, boolean required)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = numPages;
+        refNumber = "";
+        courseText = required;
+        
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    public int getBorrowed()
+    {
+        return borrowed;
+    }
+    
+    public boolean getCourseText()
+    {
+        return courseText;
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    public void raiseBorrowed()
+    {
+        borrowed += 1;
+    }
+    
+    public void printAuthor()
+    {
+        System.out.println("Author: " + author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println("Title: " + title);
+    }
+    
+    public void printDetails()
+    {
+        System.out.print("Author: " + author + 
+        ", Title: " + title + ", Pages: " + pages + 
+        ", Times Borrowed: " + borrowed);
+        
+        if (refNumber.length() > 0)
+        {
+            System.out.println(", Reference Number: " + refNumber);
+        }
+        else System.out.println(", Reference Number: zzz");
+            
+    }
 }
